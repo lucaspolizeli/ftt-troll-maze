7219a0c9c936842c3fdfea8c96a8c7168af6802d
Lucas Polizeli <lumpolizeli@gmail.com>
2017-11-11 17:00:32 -0200

    Design da tela de login

--- oficial_JogoLabirinto_N1/oficial_JogoLabirinto_N1/Form1.Designer.cs
+++ oficial_JogoLabirinto_N1/oficial_JogoLabirinto_N1/Form1.Designer.cs
@@ -28,12 +28,139 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btLogar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.txSenha = new System.Windows.Forms.TextBox();
+            this.lblSenha = new System.Windows.Forms.Label();
+            this.btCadastro = new System.Windows.Forms.Button();
+            this.lblCadastro = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btLogar
+            // 
+            this.btLogar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btLogar.Location = new System.Drawing.Point(12, 229);
+            this.btLogar.Name = "btLogar";
+            this.btLogar.Size = new System.Drawing.Size(201, 23);
+            this.btLogar.TabIndex = 0;
+            this.btLogar.Text = "Entrar";
+            this.btLogar.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 1;
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtUsuario.Location = new System.Drawing.Point(12, 156);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(201, 20);
+            this.txtUsuario.TabIndex = 2;
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Location = new System.Drawing.Point(9, 140);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(46, 13);
+            this.lblUsuario.TabIndex = 3;
+            this.lblUsuario.Text = "Usuário:";
+            // 
+            // txSenha
+            // 
+            this.txSenha.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txSenha.Location = new System.Drawing.Point(12, 201);
+            this.txSenha.Name = "txSenha";
+            this.txSenha.PasswordChar = '*';
+            this.txSenha.Size = new System.Drawing.Size(201, 20);
+            this.txSenha.TabIndex = 4;
+            this.txSenha.UseSystemPasswordChar = true;
+            // 
+            // lblSenha
+            // 
+            this.lblSenha.AutoSize = true;
+            this.lblSenha.Location = new System.Drawing.Point(9, 185);
+            this.lblSenha.Name = "lblSenha";
+            this.lblSenha.Size = new System.Drawing.Size(41, 13);
+            this.lblSenha.TabIndex = 5;
+            this.lblSenha.Text = "Senha:";
+            // 
+            // btCadastro
+            // 
+            this.btCadastro.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btCadastro.Location = new System.Drawing.Point(138, 271);
+            this.btCadastro.Name = "btCadastro";
+            this.btCadastro.Size = new System.Drawing.Size(75, 23);
+            this.btCadastro.TabIndex = 6;
+            this.btCadastro.Text = "Cadastrar";
+            this.btCadastro.UseVisualStyleBackColor = true;
+            // 
+            // lblCadastro
+            // 
+            this.lblCadastro.AutoSize = true;
+            this.lblCadastro.Location = new System.Drawing.Point(9, 276);
+            this.lblCadastro.Name = "lblCadastro";
+            this.lblCadastro.Size = new System.Drawing.Size(128, 13);
+            this.lblCadastro.TabIndex = 7;
+            this.lblCadastro.Text = "Ainda não tem cadastro? ";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(201, 116);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(224, 308);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblCadastro);
+            this.Controls.Add(this.btCadastro);
+            this.Controls.Add(this.lblSenha);
+            this.Controls.Add(this.txSenha);
+            this.Controls.Add(this.lblUsuario);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btLogar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Trump Maze";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btLogar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.TextBox txSenha;
+        private System.Windows.Forms.Label lblSenha;
+        private System.Windows.Forms.Button btCadastro;
+        private System.Windows.Forms.Label lblCadastro;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
