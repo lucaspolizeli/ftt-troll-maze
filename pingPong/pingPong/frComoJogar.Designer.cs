56d018a8e5c43366d741e28668e3686f5e279371
Lucas Polizeli <Lucas Polizeli>
2017-11-22 10:28:16 -0200

    lol

--- pingPong/pingPong/frComoJogar.Designer.cs
+++ pingPong/pingPong/frComoJogar.Designer.cs
@@ -38,7 +38,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Snap ITC", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Navy;
-            this.label1.Location = new System.Drawing.Point(71, 9);
+            this.label1.Location = new System.Drawing.Point(70, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(398, 45);
             this.label1.TabIndex = 0;
@@ -61,6 +61,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "frComoJogar";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
