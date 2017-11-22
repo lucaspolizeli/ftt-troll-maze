04dcb5c3b0ea100286a7c048dbae8bc4837b3ced
Lucas Polizeli <Lucas Polizeli>
2017-11-22 10:29:25 -0200

    lol

--- pingPong/pingPong/frMenu.Designer.cs
+++ pingPong/pingPong/frMenu.Designer.cs
@@ -84,7 +84,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::pingPong.Properties.Resources.maxresdefault;
-            this.pictureBox1.Location = new System.Drawing.Point(9, 49);
+            this.pictureBox1.Location = new System.Drawing.Point(9, 48);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(175, 109);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
